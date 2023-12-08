c1a965d3c0a021d8622349112edf920669191ca4
Veloxal <dulransamarasinghe4@gmail.com>
2023-12-08 17:22:35 +0530

    beforeaddingimages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -55,24 +55,31 @@ function App() {
         </div>
         <div className="middle">
           <h1>Projects</h1>
-          <p>
-            Currently , my projects are small scale. I'm a 2nd year majoring in
-            Computer Science at <a href="https://www.iit.ac.lk/">IIT</a>
+          <p style={{ fontSize: "x-large" }}>
+            I'm a 2nd year majoring in Computer Science at{" "}
+            <a href="https://www.iit.ac.lk/">IIT</a>. Currently my projects are
+            small scale ones , however with improvement I do wish to move onto
+            larger projects.
           </p>
-          <button>
-            <a href="https://google.com/">Browse</a>
-          </button>
 
-          <button className="contactme">
-            <a href="dulransamarasinghe3@gmail.com">Contact Me</a>
+          <button className="button-17">
+            <a href="mailto:dulransamarasinghe3@gmail.com">Contact Me</a>
           </button>
           <div className="projects">
             <h1>
-              <a href="https://github.com/DulranSam">GitHub</a>
+              <a
+                href="https://github.com/DulranSam"
+                style={{ textAlign: "center" }}
+              >
+                Find Me On GitHub
+              </a>
             </h1>
             <div className="card">
               <label>
                 <h1>Veloxal</h1>
+                <a href="https://github.com/DulranSam/VeloxalOnlineStoreMobileApp">
+                  View
+                </a>
                 <p>
                   An mobile application that i'm developing , as i'm learning
                   Flutter
@@ -83,6 +90,9 @@ function App() {
             <div className="card">
               <label>
                 <h1>Online Store Project</h1>
+                <a href="https://github.com/DulranSam/Online-Store-Project">
+                  View
+                </a>
                 <p>
                   The project where I first learned about routing and making
                   CRUD operations to the Cluster (MongoDB) , where I also gained
@@ -93,7 +103,24 @@ function App() {
             </div>
             <div className="card">
               <label>
+                <h1>Movie Trailers</h1>
+                <a href="https://github.com/DulranSam/MovieTorrentDownload">
+                  View
+                </a>
+                <p>
+                  Project created using the MERN stack , to make CRUD operations
+                  from the frontend to handle appropriately
+                  <a href="yts.am">YTS</a>
+                </p>
+                <img alt="image of UI"></img>
+              </label>
+            </div>
+            <div className="card">
+              <label>
                 <h1>Movie Torrent Downloader</h1>
+                <a href="https://github.com/DulranSam/MovieTorrentDownload">
+                  View
+                </a>
                 <p>
                   My first MERN Project , which was created without MongoDB.
                   Using a free api provided by <a href="yts.am">YTS</a>
@@ -104,10 +131,11 @@ function App() {
             <div className="card">
               <label>
                 <h1>Friends API</h1>
-                <p>
-                  An basic API I made using Express as a mini side project
-                  <a href="yts.am">YTS</a>
-                </p>
+                <a href="https://github.com/DulranSam/VeloxalOnlineStoreMobileApp">
+                  View
+                </a>
+                <p>An basic API I made using Express as a mini side project</p>
+
                 <img alt="image of UI"></img>
               </label>
             </div>
@@ -118,8 +146,24 @@ function App() {
                   Mini Side Project created using Chat-GPT's api , specifically
                   to make a post request for frontend requests to generate
                   number of ai images requested by user
-                  <a href="yts.am">YTS</a>
                 </p>
+                <a href="https://github.com/DulranSam/VeloxalOnlineStoreMobileApp">
+                  View
+                </a>
+                <br></br>
+                <img alt="image of UI"></img>
+              </label>
+            </div>
+            <div className="card">
+              <label>
+                <h1>Crpyto Conversion Mini Project</h1>
+                <p>
+                  Created using <a href="coinapi.com">Coin Api</a>
+                </p>
+                <a href="https://github.com/DulranSam/CryptoConversionMiniProject">
+                  View
+                </a>
+                <br></br>
                 <img alt="image of UI"></img>
               </label>
             </div>
@@ -134,15 +178,22 @@ function App() {
         </div>
         <div style={{ textAlign: "center" }} className="social">
           <h1>Socials</h1>
-          <div className="card">
-            <a href="">YouTube</a>
-          </div>
-          <div className="card">
-            <a href="">Twitter</a>
-          </div>
-          <div className="card">
-            <a href="">Linkedin</a>
-          </div>
+          <br></br>
+          <br></br>
+          <a href="https://www.youtube.com/@Veloxal" className="card">
+            YouTube
+          </a>
+
+          <a href="https://twitter.com/VeloxalYT" className="card">
+            Twitter
+          </a>
+
+          <a
+            href="https://www.linkedin.com/in/dulran-samarasinghe-6258b9269/"
+            className="card"
+          >
+            Linkedin
+          </a>
         </div>
         <h1>PS : Portfolio was Created Using React :)</h1>
       </div>
